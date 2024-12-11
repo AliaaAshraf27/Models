08765eba731b850a33d8d703dd60e2a3db2a86df
Aliaa Ashraf <aliaaashraf143@gmail.com>
2024-12-11 13:30:09 +0200

    Edit in Role

--- MedicalServices/Mapping/RegisterMapping/RegisterMapping.cs
+++ MedicalServices/Mapping/RegisterMapping/RegisterMapping.cs
@@ -7,7 +7,13 @@ namespace MedicalServices.Mapping.RegisterMapping
     {
         public void RegisterMapping()
         {
-            CreateMap<RegisterDTO, User>();
+            CreateMap<RegisterDTO, User>()
+                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.UserName))
+                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Phone))
+                 .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+
         }
     }
 }
