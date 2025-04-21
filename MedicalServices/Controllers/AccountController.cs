b12049b010a4ec5417b298471bae9dea7e187a6b
حبيبه 31107120 <UG_31107120@ics.tanta.edu.eg>
2025-04-21 14:35:27 +0200

    add delete account end point

--- MedicalServices/Controllers/AccountController.cs
+++ MedicalServices/Controllers/AccountController.cs
@@ -22,18 +22,20 @@ namespace MedicalServices.Controllers
         private readonly ILoginService _loginService;
         private readonly IConfiguration _configuration;
         private readonly UserManager<User> _userManager;
+        private readonly IDeleteAccountService _deleteAccountService;
         #endregion
 
         #region Constructors
         // Constructor for injecting dependencies
         public AccountController(IMapper mapper,
-                                  IRegisterServies applicationUserServices, ILoginService loginService, IConfiguration configuration, UserManager<User> userManager)
+                                  IRegisterServies applicationUserServices, ILoginService loginService, IDeleteAccountService deleteAccountService, IConfiguration configuration, UserManager<User> userManager)
         {
             _mapper = mapper;
             _applicationUserServies = applicationUserServices;
             _loginService = loginService;
             _configuration = configuration;
             _userManager = userManager;
+            _deleteAccountService = deleteAccountService;
         }
         #endregion
 
@@ -107,6 +109,14 @@ namespace MedicalServices.Controllers
                 default:
                     return BadRequest();
             }
+        }
+
+        [HttpDelete(Router.AccountRouting.DeleteAccount)]
+        public async Task<IActionResult> DeleteAccount(int accountId)
+        {
+            var result = await _deleteAccountService.DeleteAccountAsync(accountId);
+            if (!result) return BadRequest("Failed to delete account.");
+            return Ok("Account deleted successfully.");
         }
 
         //private async Task<string> GenerateJwtToken(string email)
---
+++ MedicalServices/ServicesImplementation/DeleteAccountService.cs
@@ -0,0 +1,28 @@
+﻿using MedicalServices.DbContext;
+using MedicalServices.Services;
+
+namespace MedicalServices.ServicesImplementation
+{
+    public class DeleteAccountService : IDeleteAccountService
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public DeleteAccountService(ApplicationDbContext context)
+        {
+            _dbcontext = context;
+        }
+
+        public async Task<bool> DeleteAccountAsync(int accountId)
+        {
+            // Find the account in the database
+            var user = await _dbcontext.Users.FindAsync(accountId);
+            if (user == null) return false;
+
+            // Remove the user from the database
+            _dbcontext.Users.Remove(user);
+            await _dbcontext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
